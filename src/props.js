519c10e471ad08c247cdd9abb33e4cef7366f087
Manoj Bhat <bhatanm@gmail.com>
2020-01-07 19:51:56 +0530

    updated

---
+++ src/props.js
@@ -0,0 +1,55 @@
+import React from "react";
+import './props.css';
+const Props = () => {
+  return (
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Default</th>
+      </tr>
+      <tr>
+        <td>head</td>
+        <td>func / string</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>body</td>
+        <td>func / string</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>footer</td>
+        <td>func / string</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>classNames</td>
+        <td>{"{ overlay: string, modal: string, closeIcon: string }"}</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>closeOnEscape</td>
+        <td>bool</td>
+        <td>true</td>
+      </tr>
+      <tr>
+        <td>closeIcon</td>
+        <td>{"{ src: string alt: string }"} </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>animation</td>
+        <td>{"{ name: string duration: string }"}</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>containerId</td>
+        <td>number</td>
+        <td></td>
+      </tr>
+    </table>
+  );
+};
+
+export default Props;
